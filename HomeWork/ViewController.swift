2d0e8b7a6e9735c6ac7701f7d6d1ae7ce78bddba
Admin <admin@MAC10.local>
2019-01-22 09:44:47 +0700

    Initial Commit

---
+++ HomeWork/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HomeWork
+//
+//  Created by Admin on 22/1/2562 BE.
+//  Copyright © 2562 Worapol Likitaporn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
